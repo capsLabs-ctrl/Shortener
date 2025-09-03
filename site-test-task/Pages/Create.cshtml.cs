d6aaf5b7bfa9349890b4efb1b4960cd0edfb5760
Shunkovskiy Nikita <57nnnink@gmail.com>
2025-09-03 17:01:23 +0300

    Create site page

--- site-test-task/Pages/Create.cshtml.cs
+++ site-test-task/Pages/Create.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using site_test_task.Data;
 using site_test_task.Models;
+using site_test_task.Services;
 
 namespace site_test_task.Pages
 {
@@ -26,17 +27,34 @@ namespace site_test_task.Pages
 
         [BindProperty]
         public ShortUrl ShortUrl { get; set; } = default!;
+        public UrlShortenerService shortener = new UrlShortenerService();
 
         // For more information, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
+            Console.WriteLine("ModelState.IsValid = " + ModelState.IsValid);
+            Console.WriteLine("OriginalUrl = " + ShortUrl?.OriginalUrl);
+            if (!Uri.TryCreate(ShortUrl.OriginalUrl, UriKind.Absolute, out var uriResult)
+                || (uriResult.Scheme != Uri.UriSchemeHttp && uriResult.Scheme != Uri.UriSchemeHttps))
             {
+                ModelState.AddModelError("ShortUrl.OriginalUrl", "Введите корректный URL (http или https).");
                 return Page();
             }
 
+
+            ShortUrl.ClickCount = 0;
+            ShortUrl.CreatedAt = DateTime.Now;
+            ShortUrl.ShortCode = shortener.GenerateShortCode();
             _context.ShortUrls.Add(ShortUrl);
-            await _context.SaveChangesAsync();
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
 
             return RedirectToPage("./Index");
         }
